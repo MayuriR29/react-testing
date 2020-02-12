ecca4094ab3b4dcca883061f6925d80ce2ab2433
MayuriR29 <mayuri.gr@gmail.com>
2020-02-12 18:31:29 +0800

    initial commit for teststing with enzyme

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import CommentBox from 'components/CommentBox'
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />)
+})
+
+afterEach(() => {
+  wrapped.unmount()
+})
+
+it('has a textarea and button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
+})
+
+it('has a textarea the users can type in', () => {
+  wrapped.find('textarea').simulate('change', { target: { value: 'new comment' } })
+  wrapped.update()
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+})
